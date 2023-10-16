fda07b5cb0dcc6d3cca6b27f17932192b6b683f8
Alex <alexandrezahrai@protonmail.com>
2023-10-16 11:46:23 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "moderate-cyan": "hsl(176, 50%, 47%)",
+        "dark-cyan": "hsl(176, 72%, 28%)",
+        "dark-gray": "hsl(0, 0%, 48%)",
+      },
+      screens: {
+        lg: "992px",
+        xl: "1358px",
+      },
+      container: {
+        padding: {
+          DEFAULT: "36px",
+          md: "44px",
+        },
+      },
+      boxShadow: {
+        custom:
+          "0 100px 140px -50px rgba(0,0,0,0.25), 0 0 20px 0 rgba(0,0,0,0.03)",
+      },
+    },
+  },
+  plugins: [],
+};
